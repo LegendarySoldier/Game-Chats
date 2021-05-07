30cfe20af664441e2f1cedd9b8811f9c171b1179
Legendary Soldier <76736800+LegendarySoldier@users.noreply.github.com>
2021-05-07 20:01:27 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,18 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+var firebaseConfig = {
+      apiKey: "AIzaSyDDqgnSVLf75FkS9QB-epcT5lbUZq2Om4k",
+      authDomain: "project-mission-impossible.firebaseapp.com",
+      databaseURL: "https://project-mission-impossible-default-rtdb.firebaseio.com",
+      projectId: "project-mission-impossible",
+      storageBucket: "project-mission-impossible.appspot.com",
+      messagingSenderId: "384303134478",
+      appId: "1:384303134478:web:3745ea6c912feb0322095f"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+      //Start code
+      
+      //End code
+     
